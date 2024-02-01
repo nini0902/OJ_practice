23818d356eb67dd713a88bf2033d4729280d957c
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-02-01 11:42:49 +0800

    Create 533_防守範圍.cpp

---
+++ NTOJ/533_防守範圍.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long a,b,n;
+    cin >> a >> b >> n;
+    if((a<=n)&&(n<=b)){
+        cout << "yes" << endl;
+    }
+    else{
+        cout << "no" << endl;
+    }
+return 0;
+}
