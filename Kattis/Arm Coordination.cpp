2416a43a70d9abe5036ac156c45a0aa645e09898
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-02-01 11:25:35 +0800

    Create Arm Coordination.cpp

---
+++ Kattis/Arm Coordination.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,y,r;
+    cin >> x >> y >> r;
+    cout << x+r << " " << y+r << endl;
+    cout << x+r << " " << y-r << endl;
+    cout << x-r << " " << y-r << endl;
+    cout << x-r << " " << y+r << endl;
+return 0;
+}
