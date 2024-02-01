56491ef9f4df558c0b75558ce8a04cfd3a099eb0
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-02-01 11:43:04 +0800

    Create 534_分組活動.cpp

---
+++ NTOJ/534_分組活動.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    if(a%2==b%2){
+        cout << "yes" << endl;
+    }
+    else{
+        cout << "no" << endl;
+    }
+return 0;
+}
