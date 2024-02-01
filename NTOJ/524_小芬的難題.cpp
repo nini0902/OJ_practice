93e694492df5da55283c7565336d7c7df687c67a
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-02-01 11:38:39 +0800

    Create 524_小芬的難題.cpp

---
+++ NTOJ/524_小芬的難題.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,y,a,b;
+    cin >> x >> y;
+    a = (x+y)/2;
+    b = x-a;
+    cout << "a: " << a << ", b: " << b << endl;
+return 0;
+}
