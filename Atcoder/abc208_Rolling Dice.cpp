29da7ca70e9fcb2aafcf70abc237f19869fd9370
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-02-01 11:48:19 +0800

    Create abc208_Rolling Dice.cpp

---
+++ Atcoder/abc208_Rolling Dice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    if((b >= a)&&(b <= a*6)){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+return 0; 
+}
