57d748bba1eeee7a087c69eb75845ca337abd835
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-03-07 18:32:45 +0800

    Update a096_時間差計算.cpp

--- 113年 APCS 課程春季班/a096_時間差計算.cpp
+++ 113年 APCS 課程春季班/a096_時間差計算.cpp
@@ -2,34 +2,18 @@
 using namespace std;
 
 int main(){
-    int H,M,S,h1,h2,m1,m2,s1,s2;
-    string t1,t2,h = "0",m = "0",s = "0";
-    cin >> t1 >> t2;
+    int h,m,s,t1,t2,dif;
+    char ch;
+    cin >> h >> ch >> m >> ch >> s;
+    t1 = h*3600 + m*60 + s;
 
-    h1 = int(t1[0]-48)*10+int(t1[1]-48);
-    m1 = int(t1[3]-48)*10+int(t1[4]-48);
-    s1 = int(t1[6]-48)*10+int(t1[7]-48);
+    cin >> h >> ch >> m >> ch >> s;
+    t2 = h*3600 + m*60 + s;
 
-    h2 = int(t2[0]-48)*10+int(t2[1]-48);
-    m2 = int(t2[3]-48)*10+int(t2[4]-48);
-    s2 = int(t2[6]-48)*10+int(t2[7]-48);
-    
-    H = h2-h1, M = m2-m1, S = s2-s1;
+    dif = (86400+t2-t1)%86400;
 
-    if(H < 0) H += 24;
-    if(M < 0) M += 60, H -= 1;
-    if(H < 0) H += 24;
-    if(S < 0) S += 60, M -= 1;
-    if(M < 0) M += 60, H -= 1;
-    if(H < 0) H += 24;
-
-    if(H < 10) h = h + to_string(H);
-    else h = to_string(H);
-    if(M < 10) m = m + to_string(M);
-    else m = to_string(M);
-    if(S < 10) s = s + to_string(S);
-    else s = to_string(S);
-
-    cout << h << ":" << m << ":" << s << endl;
+    cout << setw(2) << setfill('0') << dif/3600 << ":" 
+         << setw(2) << setfill('0') << dif/60%60 << ":" 
+         << setw(2) << setfill('0') << dif%3600%60 << endl;
 return 0;
 }
