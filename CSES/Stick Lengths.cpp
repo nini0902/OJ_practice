61998f40a6d4c5b8061fd1cb2070e2de97e4bec8
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-04-14 16:27:54 +0800

    Update Stick Lengths.cpp

--- CSES/Stick Lengths.cpp
+++ CSES/Stick Lengths.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 
 int main(){
-    int n,mid,sum = 0;
+    long long n,mid,sum = 0;
     cin >> n;
-    vector<int> a(n);
+    vector<long long> a(n);
     for(int i = 0; i < n; i++) cin >> a[i];
     sort(a.begin(),a.end());
 
     mid = n/2;
-    cout << a[mid] << endl;
     for(int i = 0; i < n; i++){
         sum += abs(a[i] - a[mid]);
     }
