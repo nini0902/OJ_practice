e43e2bf36251d5ce69d41b90745fe99df18594d0
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-04-28 12:02:31 +0800

    Update Sum of Two Values.cpp

--- CSES/Sum of Two Values.cpp
+++ CSES/Sum of Two Values.cpp
@@ -1,33 +1,35 @@
 #include <bits/stdc++.h>
-typedef long long LL;
 using namespace std;
 
 int main(){
-    int n,max_index,yn = 0;
-    LL x,k;
+    int n,yn = 0,t = 1;
+    long long x;
     cin >> n >> x;
-    map<LL,int> m; //方便最後要找位置
-    vector<LL> vec; //方便找對應數字
-    set<LL> s; //去除重複 + 排序
-    
+    vector<long long> vec(n+1);
+    int l = 1, r = n;
+
     for(int i = 1; i <= n; i++){
-        cin >> k;
-        s.insert(k);
-        m[k] = i; //紀錄位置
-    } 
+        cin >> vec[i];
+    }
 
-    vec.insert(vec.end(),s.begin(),s.end());
-    max_index = vec.size()-1; //確認末位
-
-    for(auto b : s){
-        for(int i = max_index; vec[i] > b; i--){
-            if(vec[i] + b == x){
-                cout << m[vec[i]] << " " <<  m[b] << " ";
-                max_index = m[vec[i]];
-                yn = 1;
+    int m = r;
+    for(int i = 1; i < n; i++){
+        if(l >= m) break;
+        l = i, r = m;
+        while(t){
+            if(vec[l] + vec[r] == x){
+                cout << l << " " << r << " ";
+                m = r-1, yn = 1;
+                t = 0;
+            }
+            else{
+                r--;
+                if(r <= l) t = 0;
             }
         }
+        t = 1;
     }
+
     if(!yn) cout << "IMPOSSIBLE";
     cout << endl;
 return 0;
