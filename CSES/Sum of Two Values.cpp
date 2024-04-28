76769b90a8a4cefffb7bc10a29e40cbd5ad03d19
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-04-28 11:48:49 +0800

    Update Sum of Two Values.cpp

--- CSES/Sum of Two Values.cpp
+++ CSES/Sum of Two Values.cpp
@@ -6,25 +6,24 @@ int main(){
     int n,max_index,yn = 0;
     LL x,k;
     cin >> n >> x;
-    map<LL,int> m;
-    vector<LL> vec;
-    set<LL> s;
-
+    map<LL,int> m; //方便最後要找位置
+    vector<LL> vec; //方便找對應數字
+    set<LL> s; //去除重複 + 排序
+    
     for(int i = 1; i <= n; i++){
         cin >> k;
         s.insert(k);
-        m[k] = i;
+        m[k] = i; //紀錄位置
     } 
 
     vec.insert(vec.end(),s.begin(),s.end());
-    max_index = n;
+    max_index = vec.size()-1; //確認末位
 
     for(auto b : s){
-        if(max_index <= b) break;
-        for(int i = max_index; i > b; i--){
-            if(vec[i] == abs(x-b)){
+        for(int i = max_index; vec[i] > b; i--){
+            if(vec[i] + b == x){
                 cout << m[vec[i]] << " " <<  m[b] << " ";
-                max_index = i;
+                max_index = m[vec[i]];
                 yn = 1;
             }
         }
