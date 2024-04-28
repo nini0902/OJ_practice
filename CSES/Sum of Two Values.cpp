11ce7b41afcee5a40d26e3f7265402293ccce329
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-04-28 12:19:00 +0800

    Update Sum of Two Values.cpp

--- CSES/Sum of Two Values.cpp
+++ CSES/Sum of Two Values.cpp
@@ -2,29 +2,30 @@
 using namespace std;
 
 int main(){
-    int n,yn = 0,t = 1;
+    int n,yn = 0,t = 1,t1 = 1;
     long long x;
     cin >> n >> x;
     vector<long long> vec(n+1);
-    int l = 1, r = n;
+    vector<int> B(n+1,0);
+    int l = 0, r = n;
 
     for(int i = 1; i <= n; i++){
-        cin >> vec[i];
+        cin >> vec[i]; //輸入數據
     }
 
     int m = r;
-    for(int i = 1; i < n; i++){
-        if(l >= m) break;
-        l = i, r = m;
+    while(t1){
+        l++, r = m;
+        if(l >= m) t1 = 0; //如果右邊值 < 左邊值
         while(t){
             if(vec[l] + vec[r] == x){
-                cout << l << " " << r << " ";
-                m = r-1, yn = 1;
-                t = 0;
+                m = r-1, yn = 1,t = 0;
+                if(B[vec[l]]==0 && B[vec[r]]==0) cout << l << " " << r << " ";
+                B[vec[l]] = 1, B[vec[r]] = 1;
             }
             else{
                 r--;
-                if(r <= l) t = 0;
+                if(r <= l) t = 0; //右邊碰到左邊
             }
         }
         t = 1;
