f1f5a99864316c2013892cf4191dc7216c59f313
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-05-19 13:24:05 +0800

    Update Tasks and Deadlines.cpp

--- CSES/Tasks and Deadlines.cpp
+++ CSES/Tasks and Deadlines.cpp
@@ -2,7 +2,7 @@
 typedef long long LL;
 using namespace std;
 
-bool comp(const pair<LL,LL>& a, const pair<LL,LL> b){
+bool comp(const pair<LL,LL>& a, const pair<LL,LL>& b){
     return a.first < b.first;
 }
 
@@ -10,14 +10,16 @@ int main(){
     LL n,used = 0,rw = 0;
     cin >> n;
     vector<pair<LL,LL>> t(n);
+
     for(int i = 0; i < n; i++){
         cin >> t[i].first >> t[i].second;
     }
-    sort(t.begin(),t.end(),comp);
+
+    sort(t.begin(),t.end(),comp); //排大小
     
     for(int i = 0; i < n; i++){
-        used += t[i].first;
-        rw += t[i].second - used;
+        used += t[i].first; //完成該工作的時間
+        rw += t[i].second - used; //獲得的東東
     }
 
     cout << rw << endl;
