273462ff0c68d510e09f4b0a06fbed7c354477b7
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-07-01 16:17:45 +0800

    Update Grid Paths.cpp

--- CSES/Grid Paths.cpp
+++ CSES/Grid Paths.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long find_path(vector<string>& s, int n){
+const int MOD = 1e9 + 7; //題目要求的取模
+
+long long find_path(vector<vector<int>>& s, int n){
     vector<vector<long long>> dp(n,vector<long long>(n,0));
 
-    dp[0][0] = (s[0][0] == '.') ? 1:0; //如果第一個位置可走，則+1
+    dp[0][0] = (s[0][0] == 1) ? 1:0;
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            if(s[i][j] == '#') continue;
-            if(i+1 < n && s[i+1][j] == '.') { //向下走
-                dp[i+1][j] += dp[i][j];
-            }
-            if(j+1 < n && s[i][j+1] == '.') { //向右走
-                dp[i][j+1] += dp[i][j];
-            }
+            if(s[i][j] == 0) continue;
+
+            if(i > 0) dp[i][j] = (dp[i][j] + dp[i-1][j])%MOD; //向下
+            if(j > 0) dp[i][j] = (dp[i][j] + dp[i][j-1])%MOD; //向右
         }
     }
     return dp[n-1][n-1];
@@ -22,10 +21,16 @@ long long find_path(vector<string>& s, int n){
 
 int main(){
     int n;
+    char c;
     cin >> n;
-    vector<string> s(n);
+    vector<vector<int>> s(n,vector<int>(n,0));
 
-    for(int i = 0; i < n; i++) cin >> s[i];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> c;
+            s[i][j] = (c == '.') ? 1:0; 
+        }
+    }
 
     cout << find_path(s,n) << endl;
 return 0;
