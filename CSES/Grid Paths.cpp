c5fdcc66fa19715c9fb08aab63cef311cc242f56
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-07-01 15:49:58 +0800

    Update Grid Paths.cpp

--- CSES/Grid Paths.cpp
+++ CSES/Grid Paths.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int find_path(vector<string>& s, int n){
-    vector<vector<int>> dp(n,vector<int>(n,0));
+    vector<vector<long long>> dp(n,vector<long long>(n,0));
 
-    dp[0][0] = (s[0][0] == '.') ? 1:0;
+    dp[0][0] = (s[0][0] == '.') ? 1:0; //如果第一個位置可走，則+1
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             if(s[i][j] == '#') continue;
-            if(i+1 < n && s[i+1][j] == '.') {
+            if(i+1 < n && s[i+1][j] == '.') { //向下走
                 dp[i+1][j] += dp[i][j];
             }
-            if(j+1 < n && s[i][j+1] == '.') {
+            if(j+1 < n && s[i][j+1] == '.') { //向右走
                 dp[i][j+1] += dp[i][j];
             }
         }
