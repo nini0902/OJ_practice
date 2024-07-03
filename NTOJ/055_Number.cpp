259d82f786a1bb9be66bd34efa76df77b2fc201e
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-07-03 15:57:35 +0800

    Create 055_Number.cpp

---
+++ NTOJ/055_Number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int count_song(int song, vector<unsigned int>& s){
+    auto l = lower_bound(s.begin(),s.end(), song);
+    auto u = upper_bound(s.begin(), s.end(), song);
+    return u-l;
+}
+
+int main(){
+    int n, q, a;
+    cin >> n;
+    vector<unsigned int> s(n);
+    for(int i = 0; i < n; i++) cin >> s[i];
+    sort(s.begin(),s.end());
+
+    cin >> q;
+    while(q--){
+        cin >> a;
+        cout << count_song(a,s) << endl;
+    }
+    return 0;
+}
