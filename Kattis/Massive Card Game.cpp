24fe174cea119f1fb298e51b6de9b33c6c5df2cc
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-07-14 15:37:58 +0800

    Create Massive Card Game.cpp

---
+++ Kattis/Massive Card Game.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, k, l, r;;
+    cin >> n;
+    int c[n];
+    for(int i = 0; i < n; i++) cin >> c[i];
+    sort(c, c+n);
+
+    cin >> k;
+    for(int i = 0; i < k; i++){
+        cin >> l >> r;
+
+        if((l == r) && (l < c[0] || r > c[n-1])){
+            cout << 0 << endl;
+        }
+        else{
+            auto L = lower_bound(c,c+n,l); //找頭
+            auto R = upper_bound(c,c+n,r); //找尾
+
+            l = L-c, r = R-c; //把L跟R回傳的迭代器轉換成位子
+
+            cout << r-l << endl;
+        }
+    }
+    return 0;
+}
