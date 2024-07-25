275228a0c2d39e444c0bc55df9f0be9476c6d8f7
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-07-25 20:50:13 +0800

    Update Subarray Sum.cpp

--- CSES/Subarray Sum.cpp
+++ CSES/Subarray Sum.cpp
@@ -2,19 +2,14 @@
 using namespace std;
 
 int main(){
-    int n, a, nf = 0;
+    int n;
     cin >> n;
-    long long arr[n+1] = {}, dp[n+2] = {0}, M = 0;
+    long long a,M = 0,cm = 0;
 
     for(int i = 0; i < n; i++){
-        cin >> arr[i];
-    }
-
-    dp[0] = arr[0];
-    for(int i = 1; i < n; i++){
-        dp[i] = dp[i-1]+arr[i];
-        while(dp[i] - arr[nf] > dp[i]) {dp[i]-=arr[nf]; nf++;}
-        M = max(M, dp[i]);
+        cin >> a;
+        cm = max(a, cm + a);
+        if(cm > M) M = cm;
     }
 
     cout << M << endl;
