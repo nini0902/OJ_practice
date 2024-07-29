95bf4f51e35f9a6cf47d351dc75f33963c77bee6
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-07-29 16:52:51 +0800

    Create d134_Combinations.cpp

---
+++ ZJ/d134_Combinations.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N,M;
+    vector<vector<unsigned long long int>> dp;
+    while(cin >> N >> M){
+        if(N == 0 && M == 0) break;
+        dp.assign(N+1,vector<unsigned long long int>(M+1,0));
+
+        for(int i = 0; i <= N; i++) {
+            dp[i][0] = 1;
+            if(i <= M) dp[i][i] = 1;
+        }
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= min(i,M); j++){
+                //dp[i][j]表示i個中取j個有多少種取法
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                //從
+            }
+        }
+        
+        cout << N <<" things taken " << M << " at a time is " << dp[N][M] << " exactly." << endl;
+    }
+    return 0;
+}
