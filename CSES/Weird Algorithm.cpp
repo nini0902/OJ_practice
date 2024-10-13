5f6d98ca267aedc166a4c70542687cc78c02aabb
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2024-10-13 13:20:24 +0800

    Create Weird Algorithm.cpp

---
+++ CSES/Weird Algorithm.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  
+  while(n != 1){
+    cout << n << " ";
+    if(n%2 == 1) n = n*3+1;
+    else n /= 2;
+  }
+  cout << n << endl;
+  return 0;
+}
