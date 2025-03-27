530180c293e208e61a92ca73bc30b7e7a3b77956
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2025-03-27 21:39:20 +0800

    Update P_2_2 離散化.cpp

--- ap325/P_2_2 離散化.cpp
+++ ap325/P_2_2 離散化.cpp
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define N 100010
-
-int d(int n, int f[], int t[]){
-    if(n < 1) return 0;
-    vector<int> v(f,f+n);
-    sort(v.begin(), v.end());
-    t[0] = v[0];
-    int num = 1;
-    for(int i = 1; i < n; i++){
-        if(v[i]!=v[i-1]) t[num++] = v[i];
-    }
-    return num;
-}
 
 int main(){
-    int n, a[N],b[N],k;
-    cin >> n;
-    for(int i = 0; i < n; i++) cin >> a[i];
-    k = d(n,a,b);
-    for(int i = 0; i < n; i++) a[i] = lower_bound(b,b+k,a[i])-b;
-    for(int i = 0; i < n; i++) cout << a[i] << " ";
-return 0;
+  int n;
+  cin >> n;
+  set<int> s;
+  vector<int> vec;
+  
+  int num;
+  while(n--){
+    cin >> num;
+    s.insert(num);
+    vec.push_back(num);
+  }
+  
+  map<int,int> mp;
+  int cnt = 0;
+  for(auto i : s){
+    mp[i] = cnt++;
+  }
+  
+  for(auto i : vec) cout << mp[i] << " ";
+  cout << endl;
+  
+  return 0;
 }
