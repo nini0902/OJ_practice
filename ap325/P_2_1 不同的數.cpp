6d8a4f446bcee5df4de1154011a99a995bbb5ae7
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2025-03-27 21:33:13 +0800

    Update P_2_1 不同的數.cpp

--- ap325/P_2_1 不同的數.cpp
+++ ap325/P_2_1 不同的數.cpp
@@ -1,25 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define N 100010
-
-int d(int f[], int t[], int n){
-    if(n < 1) return 0;
-    vector<int> v(f,f+n);
-    sort(v.begin(), v.end());
-    t[0] = v[0];
-    int num = 1;
-    for(int i = 1; i < n; i++) if(v[i] != v[i-1]) t[num++] = v[i];
-    return num;
-}
 
 int main(){
-    int n, k;
-    int a[N], b[N];
-    cin >> n;
-    for(int i = 0; i < n; i++) cin >> a[i];
-    k = d(a,b,n);
-    cout << k << endl;
-    for(int i = 0; i < k-1; i++) cout << b[i] << " ";
-    cout << b[k-1] << endl;
-return 0;
+  int n;
+  cin >> n;
+  set<int> s;
+  
+  int num;
+  while(n--){
+    cin >> num;
+    s.insert(num);
+  }
+  
+  cout << s.size() << endl;
+  for(auto i : s) cout << i << " ";
+  cout << endl;
+  return 0;
 }
