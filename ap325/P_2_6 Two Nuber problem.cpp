cc6a664a31958d9e1555ab6bb8e2e6d9354c0c0c
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2025-03-31 21:02:07 +0800

    Update P_2_6 Two Nuber problem.cpp

--- ap325/P_2_6 Two Nuber problem.cpp
+++ ap325/P_2_6 Two Nuber problem.cpp
@@ -1,19 +1,23 @@
 #include <bits/stdc++.h>
-using namespace std;
 typedef long long LL;
+using namespace std;
 
 int main(){
-    LL m,n,k,cnt = 0;
-    cin >> m >> n >> k;
-    vector<LL> A(m);
-    vector<LL> B(n);
-    for(int i = 0; i < m; i++) cin >> A[i];
-    for(int i = 0; i < n; i++) cin >> B[i];
-    sort(A.begin(),A.end());
-    sort(B.begin(),B.end());
-    for(int i = 0; i < m; i++){
-        if(binary_search(B.begin(),B.end(),k-A[i])) cnt ++;
-    }
-    cout << cnt << endl;
-return 0;
+  LL m,n,k;
+  cin >> m >> n >> k;
+  vector<LL> A(m), B(n);
+  for(int i = 0; i < m; i++) cin >> A[i];
+  for(int i = 0; i < n; i++) cin >> B[i];
+  
+  sort(A.begin(), A.end());
+  sort(B.begin(), B.end());
+  
+  int cnt = 0;
+  for(int i = 0; i < m; i++){
+    auto it = lower_bound(B.begin(), B.end(), k - A[i]);
+    if(*it + A[i] == k) cnt++;
+  }
+  
+  cout << cnt << endl;
+  return 0;
 }
