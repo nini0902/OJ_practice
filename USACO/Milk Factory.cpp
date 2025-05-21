0dad5b4f16d1801a4e876e6352367c7a5adf11ac
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2025-05-22 06:00:44 +0800

    in_deg, out_deg, tree, dfs

--- USACO/Milk Factory.cpp
+++ USACO/Milk Factory.cpp
@@ -3,24 +3,35 @@
 using namespace std;
 
 int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
   ifstream fin("factory.in");
   ofstream fout("factory.out");
   
   int n;
   fin >> n;
-  vector<vector<int>> to(n+1);
+  vector<int> in_deg(101), out_deg(101);
   
-  int a,b;
-  while(fin >> a >> b) {
-    to[b].push_back(a);
+  for(int i = 0, a,b; i < n-1; i++) {
+    fin >> a >> b;
+    // a -> b
+    out_deg[a]++, in_deg[b]++;
   }
   
+  bool encounter_sink = false;
+  int ans = -1;
   for(int i = 1; i < n+1; i++) {
-    if(to[i].size() == n-1) {
-      fout << i << endl;
-      break;
+    if(encounter_sink && out_deg[i] == 0 && in_deg[i] > 0) {
+      ans = -1;
+      break; // there're mre than one sink which are impossible to reach each other
     }
-    if(i == n) fout << "-1\n" << endl;
+    
+    if(out_deg[i] == 0 && in_deg[i] > 0) {
+      encounter_sink = true; //encountered first sink
+      ans = i;
+    }
   }
+  
+  fout << ans << endl;
   return 0;
 }
