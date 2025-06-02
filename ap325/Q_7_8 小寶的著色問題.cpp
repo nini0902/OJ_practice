f17c09aed0339390f57b2c3889ca989573e987ed
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2025-06-03 06:13:40 +0800

    Create Q_7_8 小寶的著色問題.cpp

---
+++ ap325/Q_7_8 小寶的著色問題.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(vector<vector<int>>& adj, vector<int>& color, int start) {
+  if(color[start] == -1) color[start] = 0;
+  queue<int> q;
+  q.push(start);
+  
+  while(!q.empty()) {
+    int u = q.front(); q.pop();
+    for(int v : adj[u]) {
+      if(color[v] == -1) {
+        color[v] = 1-color[u];
+        q.push(v);
+      }
+      // find the same color in the neighbor
+      else if(color[v] == color[u]) return false;
+    }
+  }
+  return true;
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T;
+  cin >> T;
+  
+  while(T--) {
+    int n,m;
+    cin >> n >> m;
+    if(m == 0) {
+      cout << "yes\n";
+      continue;
+    }
+    vector<vector<int>> adj(n);
+    for(int i = 0,a,b; i < m; i++) {
+      cin >> a >> b;
+      adj[a].push_back(b);
+      adj[b].push_back(a);
+    }
+    
+    vector<int> color(n, -1);
+    bool ans = true;
+    for(int i = 0; i < n; i++) {
+      if(color[i] == -1) {
+        if(!check(adj, color, i)) {ans = false; break;}
+      }
+    }
+    
+    if(ans == true) cout << "yes\n";
+    else cout << "no\n";
+  }
+  return 0;
+}
