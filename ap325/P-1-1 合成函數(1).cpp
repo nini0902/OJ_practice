bdc1729c66b951ce8ea0c5362668ae35930efe3e
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2026-02-26 11:01:34 +0800

    Refactor eval function for clarity and formatting

--- ap325/P-1-1 合成函數(1).cpp
+++ ap325/P-1-1 合成函數(1).cpp
@@ -2,28 +2,31 @@
 using namespace std;
 
 int eval(){
-    int val,x,y;
-    char c;
-    if(cin>>val){
-        return val;
-    }
-
-    cin.clear();
-    cin >> c;
-
-    if(c=='f'){
-        x = eval();
-        return 2*x-1;
-    }
-    else if(c=='g'){
-        x = eval();
-        y = eval();
-        return x+2*y-3;
-    }
-    return 0;
+  int val, x,y;
+  char c;
+  
+  // 嘗試讀取數字並回傳
+  if(cin >> val){
+    return val;
+  }
+  
+  // 讀取到字元(f或g)
+  cin.clear();
+  cin >> c;
+  
+  if(c == 'f'){
+    x = eval();
+    return 2*x - 1;
+  }
+  else if(c == 'g'){
+    x = eval(), y = eval();
+    return x + 2*y - 3;
+  }
+  
+  return 0;
 }
 
 int main(){
-    cout << eval() << endl;
-return 0;
+  cout << eval() << endl;
+  return 0;
 }
